ada2e1ee27e385e1953c9280b5537437796df6fb
Emil Arzumanov <22-emil@mail.ru>
2022-02-01 19:21:42 +0500

    ...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import appStyle from './App.css';
+import {Component} from "react";
+import Input from "./components/phoneNumberInput/Input";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    state = {
+        numbersData: [
+            {id:1,number: "9528262065"},
+            {id:2,number: "9528262064"},
+            {id:3,number: "9528262063"},
+            {id:4,number: "9528262062"},
+            {id:5,number: "9528262061"}
+        ],
+        phone: ""
+    }
+
+    onChangeHandler = (text) => {
+        this.setState({
+            phone:text
+        })
+    }
+
+    handleClick = () => {
+        this.state.numbersData.push({id:this.state.numbersData.length+1,number: this.state.phone})
+        let arr = this.state.numbersData;
+        this.setState({numbersData:arr})
+        this.state.phone = "";
+    };
+
+    render() {
+        return (
+            <div className={appStyle.div}>
+                <Input name="phone"
+                       mask={this.state.mask}
+                       searchArray={this.state.numbersData}
+                       callBackFunc={this.handleClick}
+                       value={this.state.phone}
+                       onChangeHandler={this.onChangeHandler}
+                />
+            </div>
+        );
+    }
 }
-
-export default App;
---
+++ src/components/phoneNumberInput/Input.jsx
@@ -0,0 +1,68 @@
+import inputStyle from "./Input.module.css";
+
+/*
+const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+phoneNumber: Yup.string().matches(phoneRegExp, 'Phone number is not valid')
+
+const Input = (props) => {
+    return (
+        <div className={inputStyle.inputDiv}>
+            <input
+                className={inputStyle.inputText}
+                type="text"
+                value={props.value}
+                onChange={event => props.onChangeHandler(event.target.value)}
+            />
+            <input className={inputStyle.inputButton} type="button" value={"Добавить"} onClick={props.callBackFunc}/>
+            <ol>{props.searchArray.map(item => <li key={item.id}>{item.number}</li>)}</ol>
+        </div>
+    );
+};
+*/
+let checker = true;
+let searchArrayChecker = true;
+function foo(value,searhArray) {
+    let regex_pattern = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/gm;
+    let check = new RegExp(regex_pattern);
+    checker = check.test(value);
+    searchArrayChecker = false;
+    if (checker === true) {
+        searhArray.forEach(element => {
+            if (element.number === value) {
+                searchArrayChecker = true;
+            }
+        })
+    }
+    if (value === "") {
+        checker = true;
+        searchArrayChecker = true;
+    }
+}
+
+const Input = (props) => {
+    function onClickButtonFunc () {
+        props.callBackFunc();
+        checker = true;
+        searchArrayChecker = true;
+    }
+    return (
+        <div>
+            <div className={inputStyle.inputDiv}>
+                <input
+                    name={props.name}
+                    className={checker ? (searchArrayChecker ? inputStyle.inputText : inputStyle.inputYellow) : inputStyle.inputTextErr}
+                    type="text"
+                    value={props.value}
+                    onChange={event => {
+                        props.onChangeHandler(event.target.value);
+                        foo(event.target.value,props.searchArray);
+                    }}
+                />
+                <input className={inputStyle.inputButton} value={"Добавить"} type={"submit"} onClick={onClickButtonFunc}/>
+                <ol>{props.searchArray.map(item => <li key={item.id}>{item.number}</li>)}</ol>
+            </div>
+        </div>
+    )
+};
+
+export default Input;
